30075505f46cafa7c59205fa567ea9be3a9c85a0
meet-kevit <meet.madani@kevit.io>
2025-07-16 12:23:20 +0530

    Contact page working

--- con.js
+++ con.js
@@ -1,26 +1,23 @@
 let tab = document.querySelector("table").querySelector("tbody");
 let btn = document.querySelector("button");
-let ph = document.querySelector("#phone-number");
-let name = document.querySelector("#name");
-let email = document.querySelector("#email");
-let act;
-if(tab.querySelectorAll('tr').length > 0){
-    act = document.querySelector("#"+ph.value);
-    act.addEventListener('click',del(act.id));
-}
+const ph = document.querySelector("#phone-number");
+const fullname = document.querySelector("#fullname");
+const email = document.querySelector("#email");
 
-btn.addEventListener('click',call);
+btn.addEventListener('click',add);
 
-
-
-
-
-function call(){
+function add(e){
+    e.preventDefault();
     let tr = document.createElement('tr');
     let one = document.createElement('td');
     let two = document.createElement('td');
     let three = document.createElement('td');
     let op = document.createElement('td');
+
+    if(!ph.value.trim() || !email.value.trim() || !fullname.value.trim()){
+        alert("Enter all inputs");
+        return;
+    }
 
     let ebt = document.createElement('button');
     ebt.textContent = 'Edit';
@@ -29,14 +26,32 @@ function call(){
     let dbt = document.createElement('button');
     dbt.textContent = 'Delete';
     dbt.setAttribute('class','delete');
+    dbt.addEventListener('click', function () {
+        tr.remove();  
+    });
+
+    ebt.addEventListener('click',() => {
+        if(ebt.textContent === 'Edit'){
+            one.innerHTML = `<input type="text" class="tempp" placeholder="Enter new mobile number" value="${one.textContent}">`;
+           two.innerHTML = `<input type="email" class="tempp" placeholder="Enter new email" value="${two.textContent}">`;
+           three.innerHTML = `<input type="text" class="tempp" placeholder="Enter new name" value="${three.textContent}">`;
+           ebt.textContent = 'Save';
+        }
+        else
+        {
+            one.textContent = `${one.querySelector('input').value}`;
+            two.textContent = `${two.querySelector('input').value}`;
+            three.textContent = `${three.querySelector('input').value}`;
+            ebt.textContent = 'Edit';
+        }
+    });
 
     op.appendChild(ebt);
     op.appendChild(dbt);
 
     one.textContent = ph.value;
     two.textContent = email.value;
-    three.textContent = three.value;
-    dbt.setAttribute('id',ph.value);
+    three.textContent = fullname.value;
 
     tr.appendChild(one);
     tr.appendChild(two);
@@ -44,14 +59,8 @@ function call(){
     tr.appendChild(op);
 
     tab.appendChild(tr);
-}
 
-function del(id){
-    const all = tab.querySelectorAll('tr');
-    for(let i = 0;i<all.length;i++){
-        if(rows[i].cells[0].textContent === id){
-            tab.remove(all[i]);
-            break;
-        }
-    }
+    ph.value = '';
+    email.value = '';
+    fullname.value = '';
 }
