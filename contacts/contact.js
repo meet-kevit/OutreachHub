7a3327b30d364944506684daf436c4c6066f8891
meet-kevit <meet.madani@kevit.io>
2025-07-24 12:56:03 +0530

    Folders seperated

---
+++ contacts/contact.js
@@ -0,0 +1,359 @@
+// let tab = document.querySelector("table").querySelector("tbody");
+// let btn = document.querySelector("button");
+// const ph = document.querySelector("#phone-number");
+// const fullname = document.querySelector("#fullname");
+// const email = document.querySelector("#email");
+// let len = null;
+// btn.addEventListener('click',add);
+// let curr = JSON.parse(localStorage.getItem('user'));
+
+// const url = `https://68779c3edba809d901f02af6.mockapi.io/contact`;
+
+// fetch(url).
+//     then(response => {
+//         if(!response.ok){
+//             throw new Error("Error!!");
+//         }
+//         return response.json();
+//     })
+//     .then(data => {
+//         len = data.length;
+//         data.forEach(curr => {
+
+//         let tr = document.createElement('tr');
+//         let one = document.createElement('td');
+//         let two = document.createElement('td');
+//         let three = document.createElement('td');
+//         let op = document.createElement('td');
+
+//         let ebt = document.createElement('button');
+//         ebt.textContent = 'Edit';
+//         ebt.setAttribute('class','edit');
+        
+//         let view = document.createElement('button');
+//         view.textContent = 'View';
+//         view.setAttribute('class','vbt');
+//         view.addEventListener('click',() => {
+//             window.location.href = 'view.html';
+//         });
+
+//         let dbt = document.createElement('button');
+//         dbt.textContent = 'Delete';
+//         dbt.setAttribute('class','delete');
+//         dbt.setAttribute('id',curr["id"]);
+//         dbt.addEventListener('click', function () {
+//            if(confirm("Are you sure to delete this contact?")){
+
+//             fetch(`${url}/${dbt.id}`,{
+//                 method: 'DELETE'
+//             })
+//             .then(() => {
+//                 tr.remove(); 
+//             })
+//             .catch(err =>{
+//                 alert("Sorry "+err);
+//             });
+//            }
+//         });
+
+//         ebt.addEventListener('click',() => {
+//             localStorage.setItem('tempedit',JSON.stringify({ph:one.textContent,email:two.textContent,name: three.textContent,id:curr["id"]}));
+//             window.location.href = 'editc.html';
+//         });
+
+//        op.appendChild(view);
+//        op.appendChild(ebt);
+//        op.appendChild(dbt);
+
+//        one.textContent = curr["contact"];
+//        two.textContent = curr["email"];
+//        three.textContent = curr["name"];
+
+//        tr.appendChild(one);
+//        tr.appendChild(two);
+//        tr.appendChild(three);
+//        tr.appendChild(op);
+
+//        tab.appendChild(tr);
+//        });
+//     })
+
+// function add(){
+//     if(!ph.value.trim() || !email.value.trim() || !fullname.value.trim()){
+//         alert("Enter all inputs");
+//         return;
+//     }
+//     len++;
+//     const oa = {
+//         contact: ph.value,
+//         email: email.value,
+//         name: fullname.value,
+//         id: len
+//     }
+
+//     fetch(`${url}`,{
+//         method : 'POST',
+//         headers: { 'Content-Type': 'application/json' },
+//         body: JSON.stringify(oa)
+//     })
+//     .then(response => {
+//         if(!response.ok){
+//             throw new Error("Adding failed!");
+//         }
+//         return response.json();
+//     })
+//     .then(data => {
+//         alert("New data added!"+data);
+//         let tr = document.createElement('tr');
+//         let one = document.createElement('td');
+//         let two = document.createElement('td');
+//         let three = document.createElement('td');
+//         let op = document.createElement('td');
+
+//         let ebt = document.createElement('button');
+//         ebt.textContent = 'Edit';
+//         ebt.setAttribute('class','edit');
+        
+//         let view = document.createElement('button');
+//         view.textContent = 'View';
+//         view.setAttribute('class','vbt');
+//         view.addEventListener('click',() => {
+//             window.location.href = 'view.html';
+//         });
+
+//         let dbt = document.createElement('button');
+//         dbt.textContent = 'Delete';
+//         dbt.setAttribute('class','delete');
+//         dbt.setAttribute('id',curr["id"]);
+//         dbt.addEventListener('click', function () {
+//            if(confirm("Are you sure to delete this contact?")){
+
+//             fetch(`${url}/${dbt.id}`,{
+//                 method: 'DELETE'
+//             })
+//             .then(() => {
+//                 tr.remove(); 
+//             })
+//             .catch(err =>{
+//                 alert("Sorry "+err);
+//             });
+//            }
+//         });
+
+//         ebt.addEventListener('click',() => {
+//             localStorage.setItem('tempedit',JSON.stringify({ph:one.textContent,email:two.textContent,name: three.textContent,id:curr["id"]}));
+//             window.location.href = 'editc.html';
+//         });
+
+//        op.appendChild(view);
+//        op.appendChild(ebt);
+//        op.appendChild(dbt);
+
+//        one.textContent = ph.value;
+//        two.textContent = email.value;
+//        three.textContent = fullname.value;
+
+//        tr.appendChild(one);
+//        tr.appendChild(two);
+//        tr.appendChild(three);
+//        tr.appendChild(op);
+
+//        tab.appendChild(tr);
+//     })
+//     .catch(err => {
+//         alert("No add",err);
+//     });
+//     alert("complete");
+// }
+
+let token = localStorage.getItem('access_token');
+let tab = document.querySelector("table").querySelector("tbody");
+let btn = document.querySelector("button");
+const ph = document.querySelector("#phone-number");
+const fullname = document.querySelector("#fullname");
+const email = document.querySelector("#email");
+let len = null;
+btn.addEventListener('click',add);
+ const lg = document.querySelector("#lg");
+  lg.addEventListener('click',logout);
+
+const url = `http://localhost:3000/contacts`;
+
+let arg = {
+  headers: {
+    'Content-Type': 'application/json',
+    'Authorization': `Bearer ${token}` // 
+  }
+}
+
+fetch(url,arg).
+    then(response => {
+        if(!response.ok){
+            throw new Error("Error!!");
+        }
+        return response.json();
+    })
+    .then(data => {
+        len = data.length;
+        alert(JSON.stringify(data));
+        data = Array.isArray(data) ? data : [data];
+        let i = 0;
+        data.forEach(curr => {
+
+        let tr = document.createElement('tr');
+        let one = document.createElement('td');
+        let two = document.createElement('td');
+        let three = document.createElement('td');
+        let op = document.createElement('td');
+
+        let ebt = document.createElement('button');
+        ebt.textContent = 'Edit';
+        ebt.setAttribute('class','edit');
+        
+        let view = document.createElement('button');
+        view.textContent = 'View';
+        view.setAttribute('class','vbt');
+        view.addEventListener('click',() => {
+            window.location.href = 'view.html';
+        });
+
+        let dbt = document.createElement('button');
+        dbt.textContent = 'Delete';
+        dbt.setAttribute('class','delete');
+        dbt.setAttribute('id',curr.data[i].id);
+        dbt.addEventListener('click', function () {
+           if(confirm("Are you sure to delete this contact?")){
+
+            fetch(`${url}/${dbt.id}`,{
+                method: 'DELETE'
+            })
+            .then(() => {
+                tr.remove(); 
+            })
+            .catch(err =>{
+                alert("Sorry "+err);
+            });
+           }
+           alert("yes");
+        });
+
+        ebt.addEventListener('click',() => {
+            localStorage.setItem('tempedit',JSON.stringify({ph:one.textContent,email:two.textContent,name: three.textContent,id:curr["id"]}));
+            window.location.href = 'editc.html';
+        });
+
+       op.appendChild(view);
+       op.appendChild(ebt);
+       op.appendChild(dbt);
+
+       one.textContent = curr.data[i].phoneNumber;
+       two.textContent = curr.data[i].id;
+       three.textContent = curr.data[i].name;
+
+       tr.appendChild(one);
+       tr.appendChild(two);
+       tr.appendChild(three);
+       tr.appendChild(op);
+
+       tab.appendChild(tr);
+       });
+    })
+
+function add(){
+    if(!ph.value.trim() || !email.value.trim() || !fullname.value.trim()){
+        alert("Enter all inputs");
+        return;
+    }
+    len++;
+    const oa = {
+        data: [
+            {phoneNumber: ph.value},
+            {email: email.value},
+            {name: fullname.value}
+        ]
+    }
+
+    fetch(`${url}`,{
+        method : 'POST',
+        headers: { 'Content-Type': 'application/json' },
+        body: JSON.stringify(oa.data[0])
+    })
+    .then(response => {
+        if(!response.ok){
+            throw new Error("Adding failed!");
+        }
+        return response.json();
+    })
+    .then(data => {
+        alert("New data added!"+data);
+        let tr = document.createElement('tr');
+        let one = document.createElement('td');
+        let two = document.createElement('td');
+        let three = document.createElement('td');
+        let op = document.createElement('td');
+
+        let ebt = document.createElement('button');
+        ebt.textContent = 'Edit';
+        ebt.setAttribute('class','edit');
+        
+        let view = document.createElement('button');
+        view.textContent = 'View';
+        view.setAttribute('class','vbt');
+        view.addEventListener('click',() => {
+            window.location.href = 'view.html';
+        });
+
+        let dbt = document.createElement('button');
+        dbt.textContent = 'Delete';
+        dbt.setAttribute('class','delete');
+        dbt.setAttribute('id',curr.data[i].id);
+        dbt.addEventListener('click', function () {
+            alert("yes")
+           if(confirm("Are you sure to delete this contact?")){
+
+            fetch(`${url}/${dbt.id}`,{
+                method: 'DELETE'
+            })
+            .then(() => {
+                tr.remove(); 
+            })
+            .catch(err =>{
+                alert("Sorry "+err);
+            });
+           }
+        });
+
+        ebt.addEventListener('click',() => {
+            localStorage.setItem('tempedit',JSON.stringify(curr.data[i]));
+            window.location.href = 'editc.html';
+        });
+
+       op.appendChild(view);
+       op.appendChild(ebt);
+       op.appendChild(dbt);
+
+       one.textContent = ph.value;
+       two.textContent = email.value;
+       three.textContent = fullname.value;
+
+       tr.appendChild(one);
+       tr.appendChild(two);
+       tr.appendChild(three);
+       tr.appendChild(op);
+
+       tab.appendChild(tr);
+    })
+    .catch(err => {
+        alert("No add",err);
+    });
+    alert("complete");
+}
+function logout(){
+    if(confirm(localStorage.getItem('user')+" will be logged out")){
+        localStorage.removeItem('user');
+        localStorage.removeItem('access_token');
+        localStorage.removeItem('userdata');
+    }
+    alert("Logged out successfully");
+    
+}
---
+++ login/login.js
@@ -0,0 +1,41 @@
+const loginForm = document.getElementById('login-form');
+ 
+loginForm.addEventListener('submit', async (e) => {
+      e.preventDefault();
+ 
+      const username = document.getElementById('username').value.trim();
+      const password = document.getElementById('password').value.trim();
+ 
+      try {
+        const response = await fetch('http://localhost:3000/auth/login', {
+          method: 'POST',
+          headers: {
+            'Content-Type': 'application/json'
+          },
+          body: JSON.stringify({ username, password })
+        });
+ 
+        const data = await response.json();
+        console.log('Login response:', data);
+        console.log(data.access_token);
+        console.log(data.username);
+        console.log(data.role);
+          if (data) {
+          const jwtToken= data.access_token;
+          const payload = atob(jwtToken.split('.')[1]);
+          console.log("payload", payload);
+          let o = JSON.parse(payload);
+          localStorage.setItem('user', o.username);
+          localStorage.setItem('access_token', data.access_token);
+          window.location.href = './homepage.html';
+        } 
+        else {
+          alert(data.message || 'Invalid credentials');
+        }
+      } 
+      catch (error) 
+      {
+        console.error('Login error:', error);
+        alert('Failed to connect to server.');
+      }
+    });
